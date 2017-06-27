9699624bf471c52e13f48787201547572e42f12f
Nicolas Duponchel <nicolas.duponchel@gmail.com>
2017-06-27 15:13:49 +0200

    Ecran Bogie (en cours)

---
+++ app/src/main/java/app/codelab/dev/bogie/Bogie.java
@@ -0,0 +1,21 @@
+package app.codelab.dev.bogie;
+
+/**
+ * Created by Nicolas on 27/06/2017.
+ */
+
+public class Bogie {
+    private String name;
+
+    public Bogie(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
